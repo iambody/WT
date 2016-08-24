4b1c32979f886bdbf69fb25b95055fd73befc6b4
datutu <beist@sohu.com>
2016-08-24 15:28:28 +0800

    分支修改adapter

--- src/io/vtown/WeiTangApp/adapter/BasAdapter.java
+++ src/io/vtown/WeiTangApp/adapter/BasAdapter.java
@@ -5,11 +5,10 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
 public abstract class BasAdapter extends BaseAdapter {
-    //dev分支进行提交master分支测试
+
     @Override
     public int getCount() {
         return 0;
-        //
 
 //ddddddddddddddddd
 
