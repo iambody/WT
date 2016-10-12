d2a3521b2bd0548caa52c9db20d5c02b769135c1
datutu <beist@sohu.com>
2016-10-12 11:19:25 +0800

    10.11  api3.0-bean

---
+++ src/io/vtown/WeiTangApp/bean/bcomment/new_three/BHome.java
@@ -0,0 +1,11 @@
+package io.vtown.WeiTangApp.bean.bcomment.new_three;
+
+import io.vtown.WeiTangApp.bean.BBase;
+
+/**
+ * Created by datutu on 2016/10/12.
+ */
+
+public class BHome extends BBase {
+
+}
