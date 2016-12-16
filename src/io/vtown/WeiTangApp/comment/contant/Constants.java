e90c4d9c83e5ef8427bdc88230d09313650ac381
datutu <beist@sohu.com>
2016-12-16 19:08:43 +0800

    12.

--- src/io/vtown/WeiTangApp/comment/contant/Constants.java
+++ src/io/vtown/WeiTangApp/comment/contant/Constants.java
@@ -989,11 +989,7 @@ public class Constants {
      */
     public static String Integral_Detail = Host
             + "/v1/member/point/list";
-    /**
-     * 获取积分明细筛选类型
-     */
-    public static String Integral_Type = Host
-            + "/v1/member/point/get-type";
+
 
     /**
      * 店铺内进行搜索
